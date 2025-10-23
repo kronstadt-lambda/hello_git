63207de005aebdbcd34027697f28d76fa60c4033
kronstadt-lambda <kronstadt.lambda@gmail.com>
2025-10-23 11:36:44 -0500

    basic3-git.js correspond to tags

---
+++ basic3-git.js
@@ -0,0 +1,7 @@
+export function basic3Git() {
+    console.log('This is a basic3 git');
+}
+/*
+ * git tag <name>: creates a tag with the name. It is useful to mark a specific task ends or versions.
+ * git tag: show all tags.
+ */
