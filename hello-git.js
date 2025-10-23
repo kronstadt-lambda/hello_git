4444f2d8a31e8ad2f4a0116cc538dc0728850a59
kronstadt-lambda <kronstadt.lambda@gmail.com>
2025-10-22 23:25:24 -0500

    This is my first commit

---
+++ hello-git.js
@@ -0,0 +1,21 @@
+export function helloGit() { return "Hello Git!" };
+console.log(helloGit());
+
+/*
+ * Commands:
+ * git --config --global user.name: shows the name of the user
+ * git --config --global user.email: shows the email of the user
+ * git init: creates a new git repository
+ * git branch -m <name>: changes the name of the current branch. It can be named like master, main, dev, trunk.
+ * git status: shows the status of the repository
+ * git add <file>: adds a specific file to the staging area
+ * git commit -m "<message>": commits the staged files with the message
+ * git push: pushes the changes to the remote repository
+ * git remote add origin <url>: adds the remote repository
+ * git remote -v: shows the remote repositories
+ * git pull: pulls the changes from the remote repository
+ * git clone <url>: clones the repository from the remote repository
+ * git log: shows the log of the commits
+ * git branch: shows the branches of the repository
+
+ */
