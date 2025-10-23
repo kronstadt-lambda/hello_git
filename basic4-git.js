59e1dc48de82b4621f4039ab1f0447851093b243
kronstadt-lambda <kronstadt.lambda@gmail.com>
2025-10-23 12:04:57 -0500

    branch and switch command used

---
+++ basic4-git.js
@@ -0,0 +1,8 @@
+export function basic4Git() {
+    console.log('This is a basic4 git. Work in login branch');
+}
+
+/*
+ * git branch <login>: creates a new branch with the name.
+ * git switch <login>: changes to the branch.</login>
+ */
