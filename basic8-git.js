4c15a98c9437ceae393cda9bd3896beea5a481d3
kronstadt-lambda <kronstadt.lambda@gmail.com>
2025-10-23 13:50:42 -0500

    diff and merge commands for fusion with the main or dev branch

---
+++ basic8-git.js
@@ -0,0 +1,6 @@
+export const git = 'git'
+
+/*
+ * (currentbranch) git diff <branch>: shows the differences between the current branch and the branch.
+ * (dev branch) git merge <branch>: merges the branch to the current branch dev.
+ */
