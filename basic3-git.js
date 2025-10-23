4a3c49ae789398279c278f9458a2d2ac7c634107
kronstadt-lambda <kronstadt.lambda@gmail.com>
2025-10-23 11:47:29 -0500

    until now we have worked on a single branch

--- basic3-git.js
+++ basic3-git.js
@@ -2,6 +2,9 @@ export function basic3Git() {
     console.log('This is a basic3 git');
 }
 /*
- * git tag <name>: creates a tag with the name. It is useful to mark a specific task ends or versions.
+ * git tag <name>: creates a tag with the name. It is useful to mark a specific task end or version.
  * git tag: show all tags.
+ * git checkout tags/<tag name>: this command changes to the tag.
+ * git revert <hash>: this deletes the commit with the hash and creates a new commit with the changes. It is useful with bugs exist.
  */
+
