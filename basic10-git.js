80c5cb989e2027486d547b3c64537caf68099522
kronstadt-lambda <kronstadt.lambda@gmail.com>
2025-10-24 17:44:08 -0500

    push corrections

--- basic10-git.js
+++ basic10-git.js
@@ -3,6 +3,8 @@ export function basic10Git() {
 }
 
 /*
- * git remote add origin https://github.com/kronstadt-lambda/l4b-git-github.git: adds the remote repository.
+ * warning: When you create a repository in github, choose the SSH for add remote
+ * git remote add origin git@github.com:kronstadt-lambda/l4b-git-github.git: adds the remote repository.
+ * git remote set-url origin git@github.com:kronstadt-lambda/l4b-git-github.git: or this.
  * git push -u origin dev: pushes the changes to the remote repository.
  */
