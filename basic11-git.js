d0c0c57012ca1df3403863669bf792122a5014bf
kronstadtFedora <kronstadt.lambda@gmail.com>
2025-10-25 15:21:24 -0500

    git clone command

---
+++ basic11-git.js
@@ -0,0 +1,8 @@
+export function basic11Git() {
+    console.log('This is a basic11 git');
+}
+
+/*
+ * git clone git@github.com:kronstadt-lambda/l4b-git-github.git: clones the repository to the current directory.
+ * git remote -v: shows the remote repository.
+ */
