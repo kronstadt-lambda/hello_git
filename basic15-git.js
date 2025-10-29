6ea8b0bc8a3c240445a6d161b301670c29c36d7d
kronstadtFedora <kronstadt.lambda@gmail.com>
2025-10-29 00:24:23 -0500

    git pages and actions tools

--- basic15-git.js
+++ basic15-git.js
@@ -5,4 +5,5 @@ export function basic14Git() {
 /*
  * git cherry-pick <commit-hash>: applies the changes introduced by a specific commit from one branch into the current branch.
  * git rebase <branch-name>: reapplies commits on top of another base tip, allowing for a cleaner project history.
+ * git rebase -i <commit-hash>: interactively rebases commits, allowing you to edit, reorder, squash, or drop commits.
  */
---
+++ basic16-git.js
@@ -0,0 +1,16 @@
+export function basic16Git() {
+    console.log('This is a basic16 git');
+}
+
+/*
+ Git Pages: https://pages.github.com/
+ Pages is useful to host static websites directly from a GitHub repository.
+ Between its features are:
+    * Free hosting for static websites.
+    * Custom domain support.
+    * Automatic
+
+ GitHub Actions: https://docs.github.com/en/actions
+ GitHub Actions integration for continuous deployment.
+ This is useful to automate all your software workflows, now you can build, test, and deploy your code right from GitHub.
+ */
