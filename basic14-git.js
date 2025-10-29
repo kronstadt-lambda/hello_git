ac6c3a62acc872f41ebea4d40ad8c8f184fcff67
kronstadtFedora <kronstadt.lambda@gmail.com>
2025-10-28 22:16:58 -0500

    git flow: commands and reasons

---
+++ basic14-git.js
@@ -0,0 +1,14 @@
+export function basic14Git() {
+    console.log('This is a basic14 git');
+}
+
+/*
+ Gitflow Workflow: https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow
+ * git flow init: initializes a Gitflow repository.
+ * git flow feature start <feature-name>: starts a new feature branch.
+ * git flow feature finish <feature-name>: finishes a feature branch and merges it into the develop branch.
+ * git flow release start <release-name>: starts a new release branch.
+ * git flow release finish <release-name>: finishes a release branch, merges it into main and develop branches, and tags the release.
+ * git flow hotfix start <hotfix-name>: starts a new hotfix branch.
+ * git flow hotfix finish <hotfix-name>: finishes a hotfix branch, merges it into main and develop branches, and tags the hotfix.
+ */
